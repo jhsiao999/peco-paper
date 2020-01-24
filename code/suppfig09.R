1e19bcdcb222854d35cb452b103b90068f54a75b
jhsiao999 <joyce.hsiao1@gmail.com>
2020-01-24 15:23:08 -0600

    change eset to sce

--- code/suppfig09.R
+++ code/suppfig09.R
@@ -1,45 +1,70 @@
 # Supplemental Figure 9
 
-library(Biobase)
+library(SingleCellExperiment)
 library(circular)
 library(peco)
 library(cluster)
 library(ggplot2)
 
-dir <- "/project2/gilad/joycehsiao/fucci-seq"
-eset <- readRDS(file.path(dir, "data/eset-final.rds"))
+sce = readRDS("data/sce-final.rds")
+pdata = data.frame(colData(sce))
 
-# Fig 2A. Derive FUCCI-based cell cycle phase
-pca <- prcomp(cbind(pData(eset)$rfp.median.log10sum.adjust,
-                    pData(eset)$gfp.median.log10sum.adjust))
-(pca$sdev^2)/sum(pca$sdev^2)
+# Figure S9A: PAM-based classification and FUCCI scores
 
-plot(pca$x[,1], pca$x[,2], pch=16, cex=.5, xlim=c(-4, 4), ylim=c(-4,4),
-     xlab="PC1 (67%)", ylab="PC2 (33%)",
-     main = "fucci intensities PC1 vs PC2", col="gray50", axes=F)
-axis(1);axis(2)
-abline(h=0,v=0, col="gray50", lty=2)
-par(new=TRUE)
+# derive cell cycle phase
+# use PCA to rotate the axes
+pca <- prcomp(cbind(pdata$rfp.median.log10sum.adjust,
+                    pdata$gfp.median.log10sum.adjust))
 theta <- coord2rad(pca$x)
-plot(circular(theta), stack=T, shrink=1.3, cex=.5, bins=200)
 theta_final <- shift_origin(as.numeric(theta), 3*pi/4)
 
 # get PAM-based clusters
-theta_final <- shift_origin(as.numeric(theta), 3*pi/4)
-pam_res <- pam(cbind(pData(eset)$rfp.median.log10sum.adjust,
-                     pData(eset)$gfp.median.log10sum.adjust), k=3)
+pam_res <- pam(cbind(pdata$rfp.median.log10sum.adjust,
+                     pdata$gfp.median.log10sum.adjust), k=3)
 clust <- data.frame(clust=pam_res$clustering,
-                    sample_id=rownames(pData(df)))
+                    sample_id=rownames(pdata))
 
+# check cluster boundaries
+plot(theta_final, clust$clust)
+
+ggplot(data.frame(gfp=pdata$gfp.median.log10sum.adjust,
+                  rfp=pdata$rfp.median.log10sum.adjust,
+                  clust=factor(clust$clust)), aes(x=gfp, y=rfp, shape=clust)) +
+  geom_point() + theme_classic() +
+  xlab("EGFP score") +
+  ylab("mCherry score") +
+  scale_shape_manual(values=c(16,1,4),
+                     labels = c("G1", "S", "G2M"),
+                     name = "") +
+  ggtitle("")
+
+
+# Figure S9C: DAPI score distribution in each PAM-basec class
+par(mfrow = c(1,3))
+for (i in 1:3) {
+  hist(pData(eset)$dapi.median.log10sum.adjust[clust$clust==i],
+       xlab = "DAPI score", main = "", xlim = c(-1.6, .7),
+       lty = "blank", col = "gray40", breaks = 60)
+  abline(v = mean(pData(eset)$dapi.median.log10sum.adjust[clust$clust==i]) +
+           c(1, 0, -1) * sd(pData(eset)$dapi.median.log10sum.adjust[clust$clust==i]),
+         col = "orange", lty = c(2, 1, 2))
+}
+
+
+
+# Figure S9B:
+# PAM-based gene classification vs Whitfield et al. 2012 classification
 
 # check all cell cycle phase markers in Whitfield et al.
-data_quant <- readRDS(file.path(dir, "output/npreg-trendfilter-quantile.Rmd/log2cpm.quant.rds"))
-sample_ord <- rownames(pData(eset))[order(theta_final)]
+data_quant <- readRDS("data/log2cpm.quant.rds")
+sample_ord <- rownames(pdata)[order(theta_final)]
 data_quant_ord <- data_quant[,match(sample_ord,colnames(data_quant))]
 
-
-macosko <- readRDS(file.path(dir,
-                             "data/cellcycle-genes-previous-studies/cycle_genes/macosko-2015.rds"))
+# import the list of cell cycle genes in Whitfeld et al. 2012 (10.1091/mbc.02-02-0030)
+# the Whitfeld et al. 2012 list is annotated and reported in Macosko et al. 2015
+# data was downloaded from Macosko et al. 2015 (10.1016/j.cell.2015.05.002)
+# https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4481139/bin/NIHMS687993-supplement-supp_data_2.xlsx
+macosko <- readRDS("data/macosko-2015.rds")
 
 subdata_plot = do.call(rbind, lapply(1:nrow(macosko), function(g) {
   gindex = which(rownames(data_quant_ord) == macosko$ensembl[g])
@@ -59,79 +84,20 @@ subdata_plot$max_state = factor(subdata_plot$max_state,
 macosko_sub = macosko[match(subdata_plot$ensembl, macosko$ensembl),]
 table(subdata_plot$max_state, macosko_sub$phase)
 
-top100 = rownames(data_quant_ord)[order(rowVars(data_quant_ord),
-                                        decreasing = TRUE)][1:500]
+# top100 = rownames(data_quant_ord)[order(rowVars(data_quant_ord),
+#                                         decreasing = TRUE)][1:500]
 
-table(subdata_plot[as.character(subdata_plot$ensembl) %in% top100,]$max_state,
-      macosko_sub[as.character(macosko_sub$ensembl) %in% top100,]$phase)
+# table(subdata_plot[as.character(subdata_plot$ensembl) %in% top100,]$max_state,
+#       macosko_sub[as.character(macosko_sub$ensembl) %in% top100,]$phase)
 
-# get all significant cyclic genes assigend as cell cycle genes
-# get significant cyclic genes that are also classifed as cell cycle genes
-# permutation-based PVE
-perm.lowmiss <- readRDS(file.path(dir,
-                                  "output/npreg-trendfilter-quantile.Rmd/fit.trend.perm.lowmiss.rds"))
-pve.perm.lowmiss <- sapply(perm.lowmiss, "[[", "trend.pve")
-fit.quant <- readRDS(file.path(dir, "output/npreg-trendfilter-quantile.Rmd/fit.quant.rds"))
-pval_peco <- data.frame(pve=sapply(fit.quant, "[[", "trend.pve"),
-                        pval_perm = sapply(fit.quant, function(x) (1+sum(pve.perm.lowmiss > x$trend.pve))/(1+ length(pve.perm.lowmiss)) ),
-                        row.names = fdata$name[match(names(fit.quant),
-                                                     rownames(fdata))])
-pval_peco_ordered <- pval_peco[order(pval_peco$pve, decreasing = T),]
+table(subdata_plot$max_state,
+      macosko_sub$phase)
 
+# compute mis-classfication rate
+# G1
+(39+13+27)/(53+39+13+27+22)
+# S
+(14+24+25)/(8+14+14+24+25)
+# G2M
+(26+51)/(26+51+99+91+53)
 
-macosko <- readRDS(file.path(dir,
-                             "data/cellcycle-genes-previous-studies/cycle_genes/macosko-2015.rds"))
-metrics <- pval_peco_ordered$pval_perm
-cutoff <- .001
-cyclegenes <- toupper(macosko$hgnc)
-allgenes <- toupper(rownames(pval_peco_ordered))
-
-which_sig_cycle <- allgenes[metrics < cutoff][allgenes[metrics < cutoff] %in% cyclegenes]
-which_sig_cycle_genes <- data.frame(names=which_sig_cycle,
-                                    ensg=rownames(fData(eset))[match(which_sig_cycle,
-                                                                     fData(eset)$name)])
-
-all.equal(as.character(clust$sample_id), colnames(data_quant_ord))
-#subdata_plot <- do.call(rbind, lapply(1:nrow(which_sig_cycle_genes), function(g) {
-
-# top 30 of 54 significant cyclic genes also known as cell cycle genes
-subdata_plot <- do.call(rbind, lapply(1:30, function(g) {
-  gindex <- which(rownames(data_quant_ord) == which_sig_cycle_genes$ensg[g])
-  gexp <- data_quant_ord[gindex,]
-  data.frame(gexp=gexp, gindex=g, gene=which_sig_cycle_genes$names[g],
-             cell_state=as.character(clust$clust)[match(colnames(data_quant_ord),
-                                                        as.character(clust$sample_id))])
-}))
-subdata_plot$cell_state <- factor(subdata_plot$cell_state,
-                                  labels=c("G1", "S", "G2M"))
-
-subdata_plot$gene <- factor(subdata_plot$gene,
-                            levels=which_sig_cycle_genes$names,
-                            labels=which_sig_cycle_genes$names)
-ggplot(subdata_plot, aes(x=cell_state, y = gexp, fill=cell_state)) +
-  geom_boxplot() + facet_wrap(~gene, nrow=6) +
-  ylab("Quantile-normalized \n gene expression levels") + xlab("") +
-  scale_fill_manual(values=cols,
-                    name="Phase") + theme_bw()
-
-
-
-# bottom 24 of 54 significant cyclic genes also known as cell cycle genes
-subdata_plot <- do.call(rbind, lapply(31:54, function(g) {
-  gindex <- which(rownames(data_quant_ord) == which_sig_cycle_genes$ensg[g])
-  gexp <- data_quant_ord[gindex,]
-  data.frame(gexp=gexp, gindex=g, gene=which_sig_cycle_genes$names[g],
-             cell_state=as.character(clust$clust)[match(colnames(data_quant_ord),
-                                                        as.character(clust$sample_id))])
-}))
-subdata_plot$cell_state <- factor(subdata_plot$cell_state,
-                                  labels=c("G1", "S", "G2M"))
-
-subdata_plot$gene <- factor(subdata_plot$gene,
-                            levels=which_sig_cycle_genes$names,
-                            labels=which_sig_cycle_genes$names)
-ggplot(subdata_plot, aes(x=cell_state, y = gexp, fill=cell_state)) +
-  geom_boxplot() + facet_wrap(~gene, nrow=6) +
-  ylab("Quantile-normalized \n gene expression levels") + xlab("") +
-  scale_fill_manual(values=cols,
-                    name="Phase") + theme_bw()
