cc8cd9696dedf930b1543a8f2d38f152bcea6dac
jhsiao999 <joyce.hsiao1@gmail.com>
2020-01-24 14:47:23 -0600

    change eset to sce

--- code/suppfig06.R
+++ code/suppfig06.R
@@ -1,67 +1,27 @@
 # Supplemental Figure 6
 
-dir <- "/project2/gilad/joycehsiao/fucci-seq"
-
-eset <- readRDS(file.path(dir, "data/eset-final.rds"))
-
-counts <- exprs(eset)
-counts <- counts[grep("ENSG", rownames(counts)), ]
-pdata <- pData(eset)
-
-
-# Intensity batch effect ---------------------------------------------------------
-lm.rfp <- lm(rfp.median.log10sum~factor(chip_id)+factor(experiment) + factor(image_label),
-             data = pdata)
-lm.gfp <- lm(gfp.median.log10sum~factor(chip_id)+factor(experiment) + factor(image_label),
-             data = pdata)
-lm.dapi <- lm(dapi.median.log10sum~factor(chip_id)+factor(experiment) + factor(image_label),
-              data = pdata)
-
-library(car)
-aov.lm.rfp <- Anova(lm.rfp, type = "III")
-aov.lm.gfp <- Anova(lm.gfp, type = "III")
-aov.lm.dapi <- Anova(lm.dapi, type = "III")
-
-aov.lm.rfp
-aov.lm.gfp
-
+library(SingleCellExperiment)
+library(circular)
+library(peco)
+library(cluster)
 library(ggplot2)
-library(RColorBrewer)
-library(cowplot)
-pdata$experiment <- factor(pdata$experiment)
-plot_grid(
-  ggplot(pdata,
-         aes(x=experiment, y=gfp.median.log10sum,
-             fill=experiment)) +
-    geom_violin(alpha=.5) + geom_boxplot(width=.1, fill="gray90") +
-    ylab("FUCC score") + xlab("Individual") +
-    ggtitle("GFP before batch-correction") + labs(fill="experiment") +
-    theme(axis.text.x  = element_text(angle=90, vjust=1, hjust=1,size=10),
-          axis.text.y  = element_text(angle=0, vjust=0.2, size=10)) +
-    theme(legend.position="none"),
-  ggplot(pdata,
-         aes(x=experiment, y=rfp.median.log10sum, fill=experiment)) +
-    geom_violin(alpha=.5) + geom_boxplot(width=.1, fill="gray90") +
-    ylab("FUCC score") + xlab("Individual") +
-    ggtitle("RFP before batch-correction") + labs(fill="experiment") +
-    theme(axis.text.x  = element_text(angle=90, vjust=1, hjust=1,size=10),
-          axis.text.y  = element_text(angle=0, vjust=0.2, size=10)) +
-    theme(legend.position="none"),
-  ggplot(pdata,
-         aes(x=experiment, y=gfp.median.log10sum.adjust, fill=experiment)) +
-    geom_violin(alpha=.5) + geom_boxplot(width=.1, fill="gray90") +
-    ylab("FUCC score") + xlab("Individual") +
-    ggtitle("GFP after batch-correction") + labs(fill="experiment") +
-    theme(axis.text.x  = element_text(angle=90, vjust=1, hjust=1,size=10),
-          axis.text.y  = element_text(angle=0, vjust=0.2, size=10)) +
-    theme(legend.position="none"),
-  ggplot(pdata,
-         aes(x=experiment, y=rfp.median.log10sum.adjust, fill=experiment)) +
-    geom_violin(alpha=.5) + geom_boxplot(width=.1, fill="gray90") +
-    ylab("FUCC score") + xlab("Individual") +
-    ggtitle("RFP after batch-correction") + labs(fill="experiment") +
-    theme(axis.text.x  = element_text(angle=90, vjust=1, hjust=1,size=10),
-          axis.text.y  = element_text(angle=0, vjust=0.2, size=10)) +
-    theme(legend.position="none"),
-  labels=LETTERS[1:4])
+
+sce <- readRDS("data/sce-final.rds")
+pdata = data.frame(colData(sce))
+
+par(mfrow = c(2,2))
+plot(x = 10^pdata$dapi.median.log10sum, ylim = c(0, 2500), xlim = c(0, 2600),
+     y = 10^pdata$gfp.median.log10sum, pch = 16, col = "gray30", cex = .7,
+     xlab = "DAPI intensity", ylab = "EGFP intensity")
+abline(0, 1, col = "red")
+plot(x = 10^pdata$dapi.median.log10sum, ylim = c(0, 2500), xlim = c(0, 2600),
+     y = 10^pdata$rfp.median.log10sum, pch = 16, col = "gray30", cex = .7,
+     xlab = "DAPI intensity", ylab = "mCherry intensity")
+abline(0, 1, col = "red")
+hist(10^pdata$dapi.median.log10sum, breaks = 40, main = "",
+     xlab = "DAPI intensity", xlim = c(0, 2600))
+cor.test(pdata$dapi.median.log10sum.adjust,
+         pdata$gfp.median.log10sum.adjust, method = "spearman")
+cor.test(pdata$dapi.median.log10sum.adjust,
+         pdata$rfp.median.log10sum.adjust, method = "spearman")
 
