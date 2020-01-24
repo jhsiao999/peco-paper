fb25ab701dbeb0c73bf9318372349dbd843852f3
jhsiao999 <joyce.hsiao1@gmail.com>
2020-01-24 14:49:41 -0600

    change eset to sce

--- code/suppfig07.R
+++ code/suppfig07.R
@@ -1,12 +1,14 @@
 # Supplemental Figure 7
+library(ggplot2)
+library(RColorBrewer)
+library(cowplot)
 
-dir <- "/project2/gilad/joycehsiao/fucci-seq"
 
-eset <- readRDS(file.path(dir, "data/eset-final.rds"))
-
-counts <- exprs(eset)
+sce <- readRDS("data/sce-final.rds")
+counts <- assay(sce)
 counts <- counts[grep("ENSG", rownames(counts)), ]
-pdata <- pData(eset)
+pdata <- data.frame(colData(sce))
+pdata$experiment <- factor(pdata$experiment)
 
 
 # Intensity batch effect ---------------------------------------------------------
@@ -25,42 +27,38 @@ aov.lm.dapi <- Anova(lm.dapi, type = "III")
 aov.lm.rfp
 aov.lm.gfp
 
-library(ggplot2)
-library(RColorBrewer)
-library(cowplot)
-pdata$chip_id <- factor(pdata$chip_id)
 plot_grid(
   ggplot(pdata,
-         aes(x=chip_id, y=gfp.median.log10sum,
-             fill=chip_id)) +
+         aes(x=experiment, y=gfp.median.log10sum,
+             fill=experiment)) +
     geom_violin(alpha=.5) + geom_boxplot(width=.1, fill="gray90") +
     ylab("FUCC score") + xlab("Individual") +
     ggtitle("GFP before batch-correction") + labs(fill="experiment") +
-    theme(axis.text.x  = element_text(angle=45, vjust=1, hjust=1,size=10),
+    theme(axis.text.x  = element_text(angle=90, vjust=1, hjust=1,size=10),
           axis.text.y  = element_text(angle=0, vjust=0.2, size=10)) +
     theme(legend.position="none"),
   ggplot(pdata,
-         aes(x=chip_id, y=rfp.median.log10sum, fill=chip_id)) +
+         aes(x=experiment, y=rfp.median.log10sum, fill=experiment)) +
     geom_violin(alpha=.5) + geom_boxplot(width=.1, fill="gray90") +
     ylab("FUCC score") + xlab("Individual") +
     ggtitle("RFP before batch-correction") + labs(fill="experiment") +
-    theme(axis.text.x  = element_text(angle=45, vjust=1, hjust=1,size=10),
+    theme(axis.text.x  = element_text(angle=90, vjust=1, hjust=1,size=10),
           axis.text.y  = element_text(angle=0, vjust=0.2, size=10)) +
     theme(legend.position="none"),
   ggplot(pdata,
-         aes(x=chip_id, y=gfp.median.log10sum.adjust, fill=chip_id)) +
+         aes(x=experiment, y=gfp.median.log10sum.adjust, fill=experiment)) +
     geom_violin(alpha=.5) + geom_boxplot(width=.1, fill="gray90") +
     ylab("FUCC score") + xlab("Individual") +
     ggtitle("GFP after batch-correction") + labs(fill="experiment") +
-    theme(axis.text.x  = element_text(angle=45, vjust=1, hjust=1,size=10),
+    theme(axis.text.x  = element_text(angle=90, vjust=1, hjust=1,size=10),
           axis.text.y  = element_text(angle=0, vjust=0.2, size=10)) +
     theme(legend.position="none"),
   ggplot(pdata,
-         aes(x=chip_id, y=rfp.median.log10sum.adjust, fill=chip_id)) +
+         aes(x=experiment, y=rfp.median.log10sum.adjust, fill=experiment)) +
     geom_violin(alpha=.5) + geom_boxplot(width=.1, fill="gray90") +
     ylab("FUCC score") + xlab("Individual") +
     ggtitle("RFP after batch-correction") + labs(fill="experiment") +
-    theme(axis.text.x  = element_text(angle=45, vjust=1, hjust=1,size=10),
+    theme(axis.text.x  = element_text(angle=90, vjust=1, hjust=1,size=10),
           axis.text.y  = element_text(angle=0, vjust=0.2, size=10)) +
     theme(legend.position="none"),
   labels=LETTERS[1:4])
