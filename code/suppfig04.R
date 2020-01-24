6a8b56f5aba86c4af69a2981e29983bf8d171b7f
jhsiao999 <joyce.hsiao1@gmail.com>
2020-01-24 14:25:15 -0600

    change eset to sce

--- code/suppfig04.R
+++ code/suppfig04.R
@@ -1,37 +1,38 @@
 # Supplemental Figure 4
 # Quality metrics in individual cell lines
 
-dir <- "/project2/gilad/joycehsiao/fucci-seq"
+library(ggplot2)
+library(cowplot)
 
-eset <- readRDS(file.path(dir, "data/eset-final.rds"))
+sce <- readRDS("data/sce-final.rds")
 
-counts <- exprs(eset)
+counts <- assay(sce)
 counts <- counts[grep("ENSG", rownames(counts)), ]
-pdata <- pData(eset)
+pdata <- data.frame(colData(sce))
 
 log2cpm.all <- t(log2(1+(10^6)*(t(counts)/pdata$molecules)))
 
-library(ggplot2)
-library(cowplot)
 pdata$dropout <- colMeans(log2cpm.all >0)
 pdata$experiment <- factor(pdata$experiment)
 pdata$chip_id <- factor(pdata$chip_id)
-ggplot(pdata, aes(x=chip_id, y=log10(molecules),
+
+plot_grid(ggplot(pdata, aes(x=chip_id, y=log10(molecules),
                   group=chip_id, col=chip_id)) +
   geom_boxplot() + coord_flip() +
   ylab("log10 molecule count") + xlab("Individual") +
   scale_color_brewer(palette="Dark2") +
-  ggtitle("Library size and individual") + theme(legend.position="none")
+  ggtitle("Library size and individual") + theme(legend.position="none"),
 ggplot(pdata, aes(x=chip_id, y=dropout,
                 group=chip_id, col=chip_id)) +
   geom_boxplot() + coord_flip() +
   ylab("Sample detection rate") + xlab("Individual") +
   scale_color_brewer(palette="Dark2") +
-  ggtitle("Detection rate and individual") + theme(legend.position="none")
+  ggtitle("Detection rate and individual") + theme(legend.position="none"),
 ggplot(pdata, aes(x=chip_id, fill=chip_id)) +
   geom_bar() + coord_flip() +
   ylab("Singletons") + xlab("Individual") +
   scale_fill_brewer("Individual",palette="Dark2") +
-  ggtitle("Singletons and individual") + theme(legend.position="none")
+  ggtitle("Singletons and individual") + theme(legend.position="none"),
+ncol = 2, labels = LETTERS[1:3])
 
 
