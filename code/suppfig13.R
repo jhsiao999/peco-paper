0c6e2fbdd0c81ce7f412fd6b063fb52b3da04877
jhsiao999 <joyce.hsiao1@gmail.com>
2020-01-24 20:32:25 -0600

    change from eset to sce

--- code/suppfig13.R
+++ code/suppfig13.R
@@ -1,56 +1,60 @@
 # Supplemental Figure S13
 
-dir <- "/project2/gilad/joycehsiao/fucci-seq"
-eset <- readRDS(file=file.path(dir, "data/eset-final.rds"))
+library(peco)
+library(SingleCellExperiment)
 
-# import Seurat results
 inds <- c("NA19098","NA18511","NA18870","NA19101","NA18855","NA19160")
-ngenes <- c(5,10)
-eval_seurat <- lapply(1:length(inds), function(j) {
-  ind <- inds[j]
-  foo <- lapply(1:length(ngenes), function(i) {
-    ngene <- ngenes[i]
-    fl_name <- file.path(dir, paste0("data/results/finalizing/ind_",ind,
-                             "_results_overallcyclical.top",ngene,".rds"))
-    df <- readRDS(fl_name)
-    return(df$fit.test)
-  })
-  names(foo) <- ngenes
-  return(foo)
+sce <- readRDS("data/sce-final.rds")
+pdata <- data.frame(colData(sce))
+
+pred_peco <- lapply(1:length(inds), function(i) {
+  ind <- inds[i]
+  out <- readRDS(paste0("data/ourdata_peco_",
+                        ind, "_top",sprintf("%03d", 5), "genes_grids_", 100, ".rds"))
+  gfp <- pdata$gfp.median.log10sum.adjust
+  rfp <- pdata$rfp.median.log10sum.adjust
+  gfp_ind <- gfp[match(names(out$fit_test$cell_times_est)[order(out$fit_test$cell_times_est)],
+                       rownames(pdata))]
+  phase <- shift_origin(with(out$fit_test, cell_times_est[order(cell_times_est)]), origin = 3*pi/4)
+  fit_gfp <- with(out$fit_test, fit_trendfilter_generic(gfp_ind))
+  fun_gfp <- approxfun(x=as.numeric(phase),
+                       y=as.numeric(fit_gfp$trend.yy), rule=2)
+  fit_gfp$phase <- phase
+  fit_gfp$gfp_ind <- gfp_ind
+  fit_gfp$fun <- fun_gfp
+
+  rfp_ind <- rfp[match(names(out$fit_test$cell_times_est)[order(out$fit_test$cell_times_est)],
+                       rownames(pdata))]
+  fit_rfp <- with(out$fit_test, fit_trendfilter_generic(rfp_ind))
+  fun_rfp <- approxfun(x=as.numeric(phase),
+                       y=as.numeric(fit_rfp$trend.yy), rule=2)
+  fit_rfp$phase <- phase
+  fit_rfp$rfp_ind <- rfp_ind
+  fit_rfp$fun <- fun_rfp
+  return(list(fit_rfp=fit_rfp, fit_gfp=fit_gfp))
 })
-names(eval_seurat) <- inds
+names(pred_peco) <- inds
+
+mean(sapply(pred_peco, function(x) x$fit_gfp$pve))
+mean(sapply(pred_peco, function(x) x$fit_rfp$pve))
 
 
-par(mfrow=c(1,1), mar=c(5,4,3,1))
-plot(eval_seurat[[1]]$`5`$fit.seurat$S,
-     eval_seurat[[1]]$`5`$fit.seurat$G2M,
-     xlab="S score",
-     ylab="G2M score",
-     pch=16, cex=.5, axes=F, xlim=c(-1.5,1.5), ylim=c(-1.5,1.5),
-     col=c("darkgoldenrod1", "coral", "darkred")[eval_seurat[[1]]$`5`$fit.seurat$assignments])
-axis(1); axis(2)
-abline(v=0,a=0,b=1,h=0, lty=2, lwd=.5)
+par(mfrow=c(3,2), mar=c(4,2,4,1))
+for (i in 1:6) {
+  plot(x=pred_peco[[i]]$fit_gfp$phase,
+       y=pred_peco[[i]]$fit_gfp$gfp_ind, col="forestgreen",
+       ylim=c(-1.5, 1.5), cex=.5, pch=16,
+       xlab="peco phase", ylab="FUCCI score",
+       main=paste0(names(pred_peco)[i], "\n",
+                   "EGFP ", round(100*pred_peco[[i]]$fit_gfp$pve,2), "%,",
+                   " mCherry ", round(100*round(pred_peco[[i]]$fit_rfp$pve,2)), "%"),
+       axes=F)
+  axis(2); axis(1,at=c(0,pi/2, pi, 3*pi/2, 2*pi),
+                labels=c(0,expression(pi/2), expression(pi), expression(3*pi/2),
+                         expression(2*pi)))
+  abline(h=0, lty=1, col="black", lwd=.7)
+  points(x=pred_peco[[i]]$fit_rfp$phase,
+         y=pred_peco[[i]]$fit_rfp$rfp_ind, col="firebrick",
+         ylim=c(-1.5, 1.5), cex=.5, pch=16)
+}
 
-par(mfrow=c(1,1), mar=c(5,4,3,1))
-seurat.pca <- prcomp(cbind(eval_seurat[[1]]$`5`$fit.seurat$G2M,
-                           eval_seurat[[1]]$`5`$fit.seurat$S), scale=TRUE)
-seurat.cell_times_est <- as.numeric(coord2rad(cbind(seurat.pca$x[,1],seurat.pca$x[,2])))
-(seurat.pca$sdev^2)/sum(seurat.pca$sdev^2)
-plot(seurat.pca$x[,1],seurat.pca$x[,2], pch=16, cex=.5,
-     xlim=c(-4,4),ylim=c(-4,4),
-     col=c("darkgoldenrod1", "coral", "darkred")[eval_seurat[[1]]$`5`$fit.seurat$assignments],
-     axes=F, xlab="PC1 (83%)", ylab="PC2 (17%)")
-axis(1); axis(2)
-abline(v=0,h=0, lty=2, lwd=.5)
-par(new=TRUE)
-library(circular)
-plot(as.circular(eval_seurat[[1]]$`5`$fit.seurat$cell_times_est[
-  eval_seurat[[1]]$`5`$fit.seurat$assignments=="G1"]),
-  stack=T, shrink=1, cex=.5, col="darkgoldenrod1")
-points(as.circular(eval_seurat[[1]]$`5`$fit.seurat$cell_times_est[
-  eval_seurat[[1]]$`5`$fit.seurat$assignments=="S"]),
-  stack=T, shrink=1, cex=.5, col="coral")
-points(as.circular(eval_seurat[[1]]$`5`$fit.seurat$cell_times_est[
-  eval_seurat[[1]]$`5`$fit.seurat$assignments=="G2M"]),
-  stack=T, shrink=1, cex=.5, col="darkred", bins=200)
-title("Supp Fig S13")
