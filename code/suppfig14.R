550174875a6b4986823f2c550ff9d10dbdccbe2d
jhsiao999 <joyce.hsiao1@gmail.com>
2020-01-24 22:12:16 -0600

    change eset to sce

--- code/suppfig14.R
+++ code/suppfig14.R
@@ -1,190 +1,101 @@
 # Supplemental Figure S14
+# Comparison of FUCCI phase with phase assignment by Seurat and Cyclone
 
-library(Biobase)
+library(SingleCellExperiment)
 library(peco)
+sce <- readRDS("data/sce-final.rds")
+pdata <- data.frame(colData(sce))
 
-dir <- "/project2/gilad/joycehsiao/fucci-seq"
+# derive cell cycle phase
+# use PCA to rotate the axes
+pca <- prcomp(cbind(pdata$rfp.median.log10sum.adjust,
+                    pdata$gfp.median.log10sum.adjust))
+theta <- coord2rad(pca$x)
+theta_final <- shift_origin(as.numeric(theta), 3*pi/4)
+names(theta_final) = rownames(pdata)
 
-eset <- readRDS(file=file.path(dir, "data/eset-final.rds"))
-pdata <- pData(eset)
-fdata <- fData(eset)
+# get PAM-based clusters
+pam_res <- pam(cbind(pdata$rfp.median.log10sum.adjust,
+                     pdata$gfp.median.log10sum.adjust), k=3)
+clust <- data.frame(clust=pam_res$clustering,
+                    sample_id=rownames(pdata))
+plot(theta_final, clust$clust)
 
-log2cpm.quant <- readRDS(file.path(dir, "output/npreg-trendfilter-quantile.Rmd/log2cpm.quant.rds"))
-
-inds <- c("NA18511", "NA18855", "NA18870", "NA19098", "NA19101", "NA19160")
-
-
-# recat
-load(file.path(dir, paste0("analysis/revision/output/ourdata_recat.rda")))
-sample_ordered <- rownames(test_exp)[ordIndex]
-phase_pred <- seq(0, 2*pi, length.out= length(ordIndex))
-names(phase_pred) <- sample_ordered
-
-diff_recat <- data.frame(phase_ref=pData(eset)$theta[match(samples_ordered, colnames(eset))])
-diff_recat$ind <- pData(eset)$chip_id[match(names(phase_pred), colnames(eset))]
-
-diff_recat$phase_pred_rot <- rotation(ref_var= diff_recat$phase_ref, shift_var = phase_pred)
-rownames(diff_recat) <- samples_ordered
-
-diff_recat$diff_time <- circ_dist(diff_recat$phase_ref, diff_recat$phase_pred_rot)
-diff_recat$method <- "reCAT"
-diff_recat <- diff_recat[,c(1,3,4,2,5)]
-#saveRDS(diff_recat, file=file.path(dir, "analysis/revision/output/fit_diff_recat.rds"))
+# find boundaries
+b1 <- mean(max(range(theta_final[clust$clust==2])), min(range(theta_final[clust$clust==3])))
+b2 <- mean(max(range(theta_final[clust$clust==3])), max(range(theta_final[clust$clust==1])))
+b3 <- mean(min(range(theta_final[clust$clust==2])), min(range(theta_final[clust$clust==1])))
+abline(v=c(b1,b2,b3), lty=2)
 
 
-# cyclone
-diff_cyclone <- do.call(rbind, lapply(1:length(inds), function(i) {
-  out_cyclone <- readRDS(file.path(dir,
-                                   paste0("analysis/revision/output/ourdata_ind_cyclone/ourdata_cyclone_",
-                                          inds[i],".rds")))
-  phase_peco <- pdata$theta[match(rownames(out_cyclone), rownames(pdata))]
-  phase_cyclone_rot <- rotation(ref_var= phase_peco, shift_var = out_cyclone$phase_pred)
-  return(data.frame(phase_ref = phase_peco,
-                    phase_pred_rot = phase_cyclone_rot,
-                    diff_time = circ_dist(phase_peco, phase_cyclone_rot),
-                    ind = inds[i],
-                    method = "Cyclone"))
-}) )
+# Supp Fig 14A
+# Seurat and PAM-based classification obtained from FUCCI scores
+out_seurat <- readRDS("data/ourdata_phase_seurat.rds")
+phase_seurat_rot <- rotation(pdata$theta,
+                             out_seurat$phase_peco[match(rownames(pdata),
+                                                         rownames(out_seurat))])
+out_seurat$Phase <- factor(out_seurat$Phase,
+                           levels = c("G1", "S", "G2M"))
+out_seurat_ordered <- out_seurat[match(rownames(pdata), rownames(out_seurat)),]
+
+all.equal(as.character(clust$sample_id), rownames(out_seurat_ordered))
+table(out_seurat_ordered$Phase, clust$clust)
+
+# Supp Fig S14B
+# Cyclone and PAM-based classification obtained from FUCCI scores
+out_cyclone <- readRDS("data/ourdata_phase_cyclone.rds")
+phase_cyclone_rot <- rotation(pdata$theta,
+                              out_cyclone$phase_cyclone_rotated[match(rownames(pdata),
+                                                                      rownames(out_cyclone))])
+out_cyclone$phases <- factor(out_cyclone$phases,
+                             levels = c("G1", "S", "G2M"))
+out_cyclone_ordered_phases <- out_cyclone$phases[match(rownames(pdata), names(out_cyclone$phases))]
+
+all.equal(as.character(clust$sample_id), names(out_cyclone_ordered_phases))
+table(out_cyclone_ordered_phases, clust$clust)
 
 
-# Seurat
-source(file.path(dir, "peco/R/run_seurat.R"))
-library(Biobase)
-library(peco)
-eset <- readRDS(file=file.path(dir, "data/eset-final.rds"))
-fdata <- fData(eset)[grep("ENSG", rownames(fData(eset))),]
-seurat.genes <- readLines(con = file.path(dir,
-                                          "data/cellcycle-genes-previous-studies/seurat_cellcycle/regev_lab_cell_cycle_genes.txt"))
-seurat.genes <- list(s.genes=seurat.genes[1:43],
-                     g2m.genes=seurat.genes[44:97])
+# Supp Fig S14C
+# Seurat-based classification and FUCCI phase
+# seurat discrete classes vs FUCCI phase
+out_seurat2 = out_seurat$Phase[match(names(theta_final), rownames(out_seurat))]
+all.equal(rownames(out_seurat2), names(theta_final))
 
-diff_seurat <- do.call(rbind, lapply(1:length(inds), function(i) {
-  #  print(i)
-  data <- readRDS(file.path(dir,paste0("analysis/revision/output/data_training_test/ind_",
-                                       inds[i], "_data_test.rds")))
-  Y_fit <- data$log2cpm_test
-  rownames(Y_fit) <- fdata$name[match(rownames(Y_fit), rownames(fdata))]
-  fit <- run_seurat(Y_fit,
-                    s.genes=seurat.genes$s.genes,
-                    g2m.genes=seurat.genes$g2m.genes)
-  fit <- as.list(fit)
-  seurat.pca <- prcomp(cbind(fit$G2M, fit$S), scale=TRUE)
-  phase_pred <- intensity2circle(seurat.pca$x, plot.it = F, method = "trig")
-  names(phase_pred) <- colnames(Y_fit)
-  phase_ref <- pdata$theta[match(names(phase_pred), rownames(pdata))]
-  phase_pred_rot <- rotation(ref_var=phase_ref, shift_var=phase_pred)
-  return(data.frame(phase_ref = phase_ref,
-                    phase_pred_rot = phase_pred_rot,
-                    diff_time = circ_dist(phase_ref, phase_pred_rot),
-                    ind = inds[i],
-                    method = "Seurat"))
-}) )
+phases = c("G1", "S", "G2M")
+cols = as.character(yarrr::piratepal("espresso")[c(3,2,1)])
+par(mfrow = c(1,3))
+for (i in seq_along(phases)) {
+  hist(theta_final[out_seurat2==phases[i]],
+       breaks = 80, ylim = c(0,40), axes = F, ann = F, lty = "blank",
+       col = cols[i])
+  title(main = paste(sum(out_seurat2==phases[i]), "cells"))
+  axis(2); axis(1,at=c(0,pi/2, pi, 3*pi/2, 2*pi),
+                labels=c(0,expression(pi/2), expression(pi), expression(3*pi/2),
+                         expression(2*pi)))
+  abline(v=c(b1,b2,b3), lty=2)
+}
 
 
-# Oscope results
-eset <- readRDS(file=file.path(dir, "data/eset-final.rds"))
-load(file = file.path(dir, "analysis/revision/output/ourdata_oscope_366genes.rda"))
+# Supp Fig S14D
+# Cyclone-based classification and FUCCI phase
+out_cyclone2 = out_cyclone$phases[match(names(theta_final),
+                                        names(out_cyclone$phases))]
+all.equal(names(out_cyclone2), names(theta_final))
 
-samples_ordered <- colnames(DataInput)[ENIRes[["cluster2"]]]
-phase_oscope <- seq(0, 2*pi, length.out= length(ENIRes[[2]]))
-
-diff_oscope <- data.frame(phase_ref=pData(eset)$theta[match(samples_ordered, colnames(eset))])
-diff_oscope$phase_pred_rot <- rotation(ref_var= diff_oscope$phase_ref, shift_var = phase_pred)
-diff_oscope$diff_time <- circ_dist(diff_oscope$phase_ref, diff_oscope$phase_pred_rot)
-diff_oscope$ind <- pData(eset)$chip_id[match(names(phase_pred), colnames(eset))]
-diff_oscope$method <- "Oscope"
-
-
-
-
-# compre predictions between methods
-
-
-diff_cyclone <- readRDS(file=file.path(dir, "analysis/revision/output/fit_diff_cyclone.rds"))
-diff_recat <- readRDS(file=file.path(dir, "analysis/revision/output/fit_diff_recat.rds"))
-diff_peco <- readRDS(file=file.path(dir, "analysis/revision/output/fit_diff_peco.rds"))
-diff_seurat <- readRDS(file=file.path(dir, "analysis/revision/output/fit_diff_seurat.rds"))
-diff_oscope <- readRDS(file=file.path(dir, "analysis/revision/output/fit_diff_oscope.rds"))
-
-diff_peco %>% group_by(ind) %>% summarise(mn=mean(diff_time)/2/pi,
-                                          sd=sd(diff_time/2/pi)/sqrt(sum(phase_pred_rot>0)))
-
-diff_cyclone %>% group_by(ind) %>% summarise(mn=mean(diff_time)/2/pi,
-                                             sd=sd(diff_time/2/pi)/sqrt(sum(phase_pred_rot>0)))
-
-diff_seurat %>% group_by(ind) %>% summarise(mn=mean(diff_time)/2/pi,
-                                            sd=sd(diff_time/2/pi)/sqrt(sum(phase_pred_rot>0)))
-
-diff_oscope %>% group_by(ind) %>% summarise(mn=mean(diff_time)/2/pi,
-                                            sd=sd(diff_time/2/pi)/sqrt(sum(phase_pred_rot>0)))
-
-diff_recat %>% group_by(ind) %>% summarise(mn=mean(diff_time)/2/pi,
-                                           sd=sd(diff_time/2/pi)/sqrt(sum(phase_pred_rot>0)))
-
-diff_all <- rbind(diff_recat,
-                  diff_cyclone,
-                  diff_peco,
-                  diff_seurat,
-                  diff_oscope)
-
-pval_difftime <- do.call(rbind, lapply(inds, function(ind) {
-  data.frame(ind=ind,
-             p_cyclone=wilcox.test(diff_peco[diff_peco$ind==ind,]$diff_time/2/pi,
-                                   diff_cyclone[diff_cyclone$ind==ind,]$diff_time/2/pi)$p.value,
-             p_seurat=wilcox.test(diff_peco[diff_peco$ind==ind,]$diff_time/2/pi,
-                                  diff_seurat[diff_peco$ind==ind,]$diff_time/2/pi)$p.value,
-             p_oscope=wilcox.test(diff_peco[diff_peco$ind==ind,]$diff_time/2/pi,
-                                  diff_oscope[diff_peco$ind==ind,]$diff_time/2/pi)$p.value,
-             p_recat=wilcox.test(diff_peco[diff_peco$ind==ind,]$diff_time/2/pi,
-                                 diff_recat[diff_peco$ind==ind,]$diff_time/2/pi)$p.value,
-             ratio_cyclone=mean(diff_peco[diff_peco$ind==ind,]$diff_time)/mean(diff_cyclone[diff_cyclone$ind==ind,]$diff_time),
-             ratio_seurat=mean(diff_peco[diff_peco$ind==ind,]$diff_time)/mean(diff_seurat[diff_seurat$ind==ind,]$diff_time),
-             ratio_oscope=mean(diff_peco[diff_peco$ind==ind,]$diff_time)/mean(diff_oscope[diff_oscope$ind==ind,]$diff_time),
-             ratio_recat=mean(diff_peco[diff_peco$ind==ind,]$diff_time)/mean(diff_recat[diff_recat$ind==ind,]$diff_time))
-}))
-cbind(inds, round(pval_difftime[,-1],4))
-
-mean(pval_difftime$ratio_cyclone)
-mean(pval_difftime$ratio_seurat)
-mean(pval_difftime$ratio_oscope)
-mean(pval_difftime$ratio_recat)
-
-# peco results
-inds <- c("NA19098","NA18511","NA18870","NA19101","NA18855","NA19160")
-ngene <- 5
-diff_peco <- do.call(rbind, lapply(1:length(inds), function(i) {
-  ind <- inds[i]
-  fl_name <- list.files(file.path(dir, "data/results/finalizing"),
-                        pattern=paste0("ind_",ind,"_results_overallcyclical.top",ngene,".rds"),
-                        full.names = TRUE)
-  df <- readRDS(fl_name)
-  df_pred <- df$fit.test$fit.supervised
-  return(data.frame(phase_ref = df_pred$ref_time,
-                    phase_pred_rot = df_pred$pred_time_shift,
-                    diff_time = df_pred$diff_time,
-                    ind = ind,
-                    method = "peco"))
-}))
+phases = c("G1", "S", "G2M")
+cols = as.character(yarrr::piratepal("espresso")[c(3,2,1)])
+par(mfrow = c(1,3))
+for (i in seq_along(phases)) {
+  hist(theta_final[out_cyclone2==phases[i]],
+       breaks = 80, ylim = c(0,40), axes = F, ann = F, lty = "blank",
+       col = cols[i])
+  title(main = paste(sum(out_cyclone2==phases[i]), "cells"))
+  axis(2); axis(1,at=c(0,pi/2, pi, 3*pi/2, 2*pi),
+                labels=c(0,expression(pi/2), expression(pi), expression(3*pi/2),
+                         expression(2*pi)))
+  abline(v=c(b1,b2,b3), lty=2)
+}
 
 
 
-diff_cyclone <- readRDS(file=file.path(dir, "analysis/revision/output/fit_diff_cyclone.rds"))
-diff_recat <- readRDS(file=file.path(dir, "analysis/revision/output/fit_diff_recat.rds"))
-diff_peco <- readRDS(file=file.path(dir, "analysis/revision/output/fit_diff_peco.rds"))
-diff_seurat <- readRDS(file=file.path(dir, "analysis/revision/output/fit_diff_seurat.rds"))
-diff_oscope <- readRDS(file=file.path(dir, "analysis/revision/output/fit_diff_oscope.rds"))
 
-diff_peco %>% group_by(ind) %>% summarise(mn=mean(diff_time)/2/pi,
-                                          sd=sd(diff_time/2/pi)/sqrt(sum(phase_pred_rot>0)))
-
-diff_cyclone %>% group_by(ind) %>% summarise(mn=mean(diff_time)/2/pi,
-                                             sd=sd(diff_time/2/pi)/sqrt(sum(phase_pred_rot>0)))
-
-diff_seurat %>% group_by(ind) %>% summarise(mn=mean(diff_time)/2/pi,
-                                            sd=sd(diff_time/2/pi)/sqrt(sum(phase_pred_rot>0)))
-
-diff_oscope %>% group_by(ind) %>% summarise(mn=mean(diff_time)/2/pi,
-                                            sd=sd(diff_time/2/pi)/sqrt(sum(phase_pred_rot>0)))
-
-diff_recat %>% group_by(ind) %>% summarise(mn=mean(diff_time)/2/pi,
-                                           sd=sd(diff_time/2/pi)/sqrt(sum(phase_pred_rot>0)))
