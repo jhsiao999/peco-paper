9d6d100bab3d3d69adaa81031d9ab2677b151da8
jhsiao999 <joyce.hsiao1@gmail.com>
2020-01-26 17:44:58 -0600

    minor edits

--- code/suppfig05.R
+++ code/suppfig05.R
@@ -1,93 +1,41 @@
 # Supplemental Figure S5
+#   Major sources of variation in our gene expression data of 888 quality samples and 11,040 genes.
 
-library("cowplot")
-library("dplyr")
-library("edgeR")
-library("ggplot2")
-library("heatmap3")
-library("reshape2")
-library("SingleCellExperiment")
+library(cowplot)
+library(dplyr)
+library(edgeR)
+library(ggplot2)
+library(heatmap3)
+library(reshape2)
+library(SingleCellExperiment)
 library(heatmap3)
 library(RColorBrewer)
 
-#' @title Perform Principal Components Analysis (PCA).
-#
-#' @param x gene-by-sample matrix
-#' @param retx center, scale - see ?prcomp
-#
-#' @return a list with the following elements:
-#   PCs - sample-by-PC matrix of principal components
-#   explained - proportion of variance explained by each PC
-#
-#' @references Zhang et al. 2009 (http://www.ncbi.nlm.nih.gov/pubmed/19763933)
-#'
-#' @export
-run_pca <- function(x, retx = TRUE, center = TRUE, scale = TRUE) {
-  library("testit")
+# load code to apply PCA to gene expression data
+source("code/utility.R")
 
-  pca <- prcomp(t(x), retx = retx, center = center, scale. = scale)
-  variances <- pca$sdev^2
-  explained <- variances / sum(variances)
-  assert("Variance explained is calculated correctly.",
-         explained[1:2] - summary(pca)$importance[2, 1:2] < 0.0001)
-  return(list(PCs = pca$x, explained = explained))
-}
 
-#' @title Plot PCA results.
-#
-#' @param x numeric matrix of PCs
-#' @param pcx PC to plot on x-axis (default: 1)
-#' @param pcy PC to plot on y-axis (default: 2)
-#' @param explained numeric vector of fractions of variance explained by each PC
-#' @param metadata data frame or matrix that contains the metadata used to annotate
-#             the plot
-#' @param color, shape, size: column name of metadata used to pass column to ggplot
-#                       aesthetic
-#' @param factors character vector which contains the column names of metadata that
-#            need to be explicitly converted to a factor
-#' @param ...  Additional arguments passed to geom_point
-#'
-#' @export
-plot_pca <- function(x, pcx = 1, pcy = 2, explained = NULL, metadata = NULL,
-                     color = NULL, shape = NULL, factors = NULL,
-                     ...) {
-  library("ggplot2")
-  library("testit")
+sce_filtered <- readRDS("data/sce-filtered.rds")
+counts <- assay(sce_filtered)
+pdata <- data.frame(colData(sce_filtered))
+pdata$experiment <- as.factor(pdata$experiment)
 
-  # Prepare data frame to pass to ggplot
-  if (!is.null(metadata)) {
-    assert("PC and metadata have same number of rows.",
-           nrow(x) == nrow(metadata))
-    plot_data <- cbind(x, metadata)
-    plot_data <- as.data.frame(plot_data)
-    # Convert numeric factors to class "factor"
-    for (f in factors) {
-      plot_data[, f] <- as.factor(plot_data[, f])
-    }
-  } else {
-    plot_data <- as.data.frame(x)
-  }
-  # Prepare axis labels
-  if (!is.null(explained)) {
-    assert("Number of PCs differs between x and explained.",
-           length(explained) == ncol(x))
-    xaxis <- sprintf("PC%d (%.2f%%)", pcx, round(explained[pcx] * 100, 2))
-    yaxis <- sprintf("PC%d (%.2f%%)", pcy, round(explained[pcy] * 100, 2))
-  } else {
-    xaxis <- paste0("PC", pcx)
-    yaxis <- paste0("PC", pcy)
-  }
-  # Plot
-  p <- ggplot(plot_data, aes_string(x = paste0("PC", pcx),
-                                    y = paste0("PC", pcy),
-                                    color = color,
-                                    shape = shape)) +
-    geom_point(...) +
-    labs(x = xaxis, y = yaxis)
-  p
-}
+# Compute log2 CPM based on the library size before filtering.
+log2cpm <- edgeR::cpm(counts, log = TRUE)
+pca_log2cpm <- run_pca(log2cpm)
 
-## a function of pca vs technical factors
+
+# selection of technical factor
+covariates <- pdata %>% dplyr::select(experiment, well, chip_id,
+                                            concentration, raw:unmapped,
+                                            starts_with("detect"),  molecules)
+
+
+
+# Supp Fig S5A: Association between Principal Components of gene e
+#   expression data and technical factors.
+
+# R-square between PCs and the covariates
 get_r2 <- function(x, y) {
   stopifnot(length(x) == length(y))
   model <- lm(y ~ x)
@@ -95,31 +43,10 @@ get_r2 <- function(x, y) {
   return(stats$adj.r.squared)
 }
 
-
-## PCA
-sce_filtered <- readRDS("data/sce-filtered.rds")
-counts = assay(sce_filtered)
-pdata <- data.frame(colData(sce_filtered))
-pdata$experiment <- as.factor(pdata$experiment)
-
-# Compute log2 CPM based on the library size before filtering.
-log2cpm <- edgeR::cpm(counts, log = TRUE)
-
-pca_log2cpm <- run_pca(log2cpm)
-
-
-# PCA variation -------------------------------------------------------------------
-# selection of technical factor
-
-covariates <- pdata %>% dplyr::select(experiment, well, chip_id,
-                                            concentration, raw:unmapped,
-                                            starts_with("detect"),  molecules)
 # look at the first 6 PCs
 pca_log2cpm <- prcomp(t(log2cpm), scale. = TRUE, center = TRUE)
 pcs <- pca_log2cpm$x[, 1:6]
 
-
-# R-square between PCs and the covariates
 r2 <- matrix(NA, nrow = ncol(covariates), ncol = ncol(pcs),
              dimnames = list(colnames(covariates), colnames(pcs)))
 for (cov in colnames(covariates)) {
@@ -128,7 +55,6 @@ for (cov in colnames(covariates)) {
   }
 }
 
-# plot heatmap
 heatmap3(r2, cexRow=1, cexCol=1, margins=c(4,15), scale="none",
          ylab="", main = "", symm=F,
          Colv=NA, showColDendro = F,
@@ -158,7 +84,7 @@ summary(f2)$adj.r.squared
 100*((pca_log2cpm$sdev^2)/sum(pca_log2cpm$sdev^2))[1:6]
 
 
-# Correlation between Technical factors
+# Supp Fig S5B: Correlation between Technical factors
 cor_tech <- cor(as.matrix(covariates[,4:11]),use="pairwise.complete.obs")
 heatmap3(cor_tech, symm = TRUE, margins=c(7,15),
          col=brewer.pal (9, "Blues" ), cexRow=1, cexCol=1, scale="none",
