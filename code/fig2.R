0e12fca1c035b03635d662067d263e8dc5f098a5
jhsiao999 <joyce.hsiao1@gmail.com>
2020-01-26 12:56:34 -0600

    cleaning up

--- code/fig2.R
+++ code/fig2.R
@@ -1,35 +1,36 @@
 # Figure 2
+# Characterizing cell cycle phase using FUCCI fluorescence intensities.
 
-library(SingleCellExperiment)
-library(circular)
-library(peco)
-library(cluster)
-library(ggplot2)
 
-dir <- "/project2/gilad/joycehsiao/fucci-seq"
-sce <- readRDS(file.path(dir, "data/sce-final.rds"))
+ssce <- readRDS("data/sce-final.rds")
+
+
+
 
 # Fig 2A. Derive FUCCI-based cell cycle phase
-pca <- prcomp(cbind(colData(sce)$rfp.median.log10sum.adjust,
-                    colData(sce)$gfp.median.log10sum.adjust))
-(pca$sdev^2)/sum(pca$sdev^2)
-
-plot(pca$x[,1], pca$x[,2], pch=16, cex=.5, xlim=c(-4, 4), ylim=c(-4,4),
-     xlab="PC1 (67%)", ylab="PC2 (33%)",
-     main = "fucci intensities PC1 vs PC2", col="gray50", axes=F)
+ints <- data.frame(rfp=colData(sce)$rfp.median.log10sum.adjust,
+                   gfp=colData(sce)$gfp.median.log10sum.adjust)
+plot(x=ints$rfp, y=ints$gfp, pch=16, cex=.5, xlim=c(-1.5, 1.5), ylim=c(-1.5,1.5),
+     xlab="eCherry score", ylab="EGFP score",
+     main = "FUCCI scores", col="gray50", axes=F)
 axis(1);axis(2)
 abline(h=0,v=0, col="gray50", lty=2)
 par(new=TRUE)
-theta <- coord2rad(pca$x)
+theta <- coord2rad(scale(ints))
 plot(circular(theta), stack=T, shrink=1.3, cex=.5, bins=200)
-theta_final <- shift_origin(as.numeric(theta), 3*pi/4)
+
 
 # Fig 2B. FUCCI phase summarizes fluorescent intensities
 
+# Compute FUCCI phase, do a PCA rotation
+ints <- data.frame(rfp=colData(sce)$rfp.median.log10sum.adjust,
+                   gfp=colData(sce)$gfp.median.log10sum.adjust)
+pca <- prcomp(ints)
+theta <- coord2rad(pca$x)
+theta_final <- shift_origin(as.numeric(theta), origin = 3*pi/4)
+
 # get PAM-based clusters
-theta_final <- shift_origin(as.numeric(theta), 3*pi/4)
-pam_res <- pam(cbind(colData(sce)$rfp.median.log10sum.adjust,
-                     colData(sce)$gfp.median.log10sum.adjust), k=3)
+pam_res <- pam(ints, k=3)
 clust <- data.frame(clust=pam_res$clustering,
                     sample_id=rownames(colData(sce)))
 plot(theta_final, clust$clust)
@@ -42,8 +43,8 @@ abline(v=c(b1,b2,b3), lty=2)
 # clust 3 = G2/M
 
 par(mfrow=c(1,1))
-plot(x=shift_origin(as.numeric(theta), 3*pi/4),
-     y=colData(sce)$gfp.median.log10sum.adjust, col="forestgreen",
+plot(x=theta_final,
+     y=ints$gfp, col="forestgreen",
      pch=c(16,1,4)[clust$clust],
      ylim=c(-1.5, 1.5), cex=.5,
      xlab="FUCCI phase", ylab="FUCCI scores",
@@ -52,8 +53,8 @@ axis(2); axis(1,at=c(0,pi/2, pi, 3*pi/2, 2*pi),
               labels=c(0,expression(pi/2), expression(pi), expression(3*pi/2),
                        expression(2*pi)))
 abline(h=0, lty=1, col="black", lwd=.7)
-points(x=shift_origin(as.numeric(theta), 3*pi/4),
-       y=colData(sce)$rfp.median.log10sum.adjust, col="firebrick",
+points(x=theta_final,
+       y=ints$rfp, col="firebrick",
        ylim=c(-1.5, 1.5),
        pch=c(16,1,4)[clust$clust],
        cex=.5)
@@ -69,7 +70,7 @@ genes <- names(pve_all_ord)[1:5]
 labs <- c("CDK1", "UBE2C", "TOP2A", "HIST1H4E", "HIST1H4C")
 
 data_quant <- readRDS("data/log2cpm.quant.rds")
-sample_ord <- rownames(colData(df))[order(theta_final)]
+sample_ord <- rownames(colData(sce))[order(theta_final)]
 data_quant_ord <- data_quant[,match(sample_ord,colnames(data_quant))]
 fits_tmp <- lapply(1:5, function(i) {
   ii <- which(rownames(data_quant_ord)==genes[i])
